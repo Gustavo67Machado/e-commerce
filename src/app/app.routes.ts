e3c4ef624fa5e21e05db307d7e3fda76e05cf2a4
gustavo <tadeboa793@gmail.com>
2026-07-21 17:04:26 -0300

    feat:criado uma nova rota para carrinho, lista produtos e home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
+// //import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos'; import Legado
+// import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// import {Home} from './features/home/home/home';
 
-export const routes: Routes = [];
+// export const routes: Routes = [
+// {
+//     path: '',
+//     component: Home,
+// },
+// //código legado com LazyLoading
+// // {
+// //     path:'produtos',
+// //     component: ListaProdutos,
+// // },
+
+// {
+//     path: 'carrinho',
+//     component: Carrinho,
+// },
+// ];
+//! codígo final LazyLoading e LoadingComponent
+import { Routes } from "@angular/router";
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent: () =>
+            import('./features/home/home/home').then((m) => m.Home),
+    },
+    {
+        path:'produtos',
+        loadComponent: () =>
+            import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
+    },
+    {
+        path:'carrinho',
+        loadComponent: () =>
+            import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+    },
+    {
+        path:'**',
+        redirectTo: '',
+    },
+];
