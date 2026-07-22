cd6e490b52b051c6341fe82fbb36e12fd7ed4147
gustavo machado <tadeboa793@gmail.com>
2026-07-22 16:09:42 -0300

    feat: adicionado as funções login, logout e usuarioLogado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router'; 
+import { usuarioLogado, login, logout } from './core/auth';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Lucia Santa'; //nome do e-commerce
+  nomeLoja = 'Lucia Santa'; //nome do e-
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
+
 }
