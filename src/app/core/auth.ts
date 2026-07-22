1056d815e76c9fe8e83a8bcf022d86320eec0066
gustavo machado <tadeboa793@gmail.com>
2026-07-22 16:13:08 -0300

    chore: criado duas pastas para a autênticação do usuario

---
+++ src/app/core/auth.ts
@@ -0,0 +1,12 @@
+import { signal } from "@angular/core";
+
+//! Define valor inicial do signal usuarioLogado com (false)
+export const usuarioLogado = signal (false);
+//!Define Signal usuarioLogado como (true), permite acesso as rotas
+export function login(){
+    usuarioLogado.set(true);
+}
+//!Define Signal usuarioLogado com (false), bloqueio acesso imediatamente
+export function logout(){
+    usuarioLogado.set(false);
+}
