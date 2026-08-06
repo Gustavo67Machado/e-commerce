fafbc934e87dc34bba5035dd5076b16f0d2e8409
gustavo machado <tadeboa793@gmail.com>
2026-08-06 15:11:00 -0300

    feat: adicionado o private carrinhoService e quantidade

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { CarrinhoService } from '../../../core/service/carrinho.service';
+CarrinhoService
+
 @Component({
   selector: 'app-header',
   imports: [MatButtonModule, MatToolbarModule, RouterLink, MatIconModule],
@@ -11,4 +14,6 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class Header {
   nomeLoja = 'Lucia Santa';
+  private carrinhoService = inject(CarrinhoService);
+  quantidade = this.carrinhoService.quantidadedeItens;
 }
