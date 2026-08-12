fbb1aabd15ae4e82b65b7a1a8efb1451a176760a
gustavo machado <tadeboa793@gmail.com>
2026-08-12 17:24:13 -0300

    feat: abd

---
+++ src/app/core/admin.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { CanActivateFn } from "@angular/router";
+import { AuthService } from "./service/auth.service";
+
+export const adimnGuard: CanActivateFn = () => {
+
+    const router = inject(Router);
+    const authService = inject(AuthService)
+
+//! - 1) Verifecar se o usuario esta logado
+if(!authService.usuarioLogado()){
+    return router.createUrlTree(['/login']);
+    }
+    //! - 2) Verificar se o usuario atual (Logado), se tem perfil adm
+    if(!authService.admin()){
+        return router.createUrlTree(['/acesso-negado'])
+    }
+    //! - 3) se o usuario estiver logado e for adm = ACESSO LIBERADO
+    return true;
+};
