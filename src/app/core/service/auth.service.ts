dfc14d8320b845b7957611c0a9f56a0f36a8c8ce
gustavo machado <tadeboa793@gmail.com>
2026-08-12 17:20:53 -0300

    feat: adicionado a const de perfilUsuario

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, computed, signal } from "@angular/core";
 
+type PerfilUsuario = 'admin' | 'usuario'; 
+
 type Usuario = {
     email: string;
-    perfil: 'usuario';
+    perfil: PerfilUsuario;
 }
 
 @Injectable({
@@ -10,18 +12,22 @@ type Usuario = {
 })
 
 export class AuthService {
+
+
     private usuario = signal<Usuario | null> (null);
     private tokenJwt = signal<string | null> (null);
 
 
     usuarioAtual = computed(() => this.usuario());
-    token = computed(() => this.tokenJwt());
     usuarioLogado = computed(() => this.usuario() !== null);
+    token = computed(() => this.tokenJwt());
+    admin = computed(() => this.usuario()?.perfil ==='admin');
 
     login (email: string, senha: string): boolean{
         if(!email || !senha){
             return false;
         }
+        const perfil: PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario'
         const tokenSimulado = 
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
     'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
@@ -30,7 +36,7 @@ export class AuthService {
     
     this.usuario.set({
         email, 
-        perfil: 'usuario',
+        perfil,
     });
     this.tokenJwt.set(tokenSimulado);
     return true;
