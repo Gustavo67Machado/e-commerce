ea4761ba0896a6c158b21ec22aa2c2528210d382
gustavo machado <tadeboa793@gmail.com>
2026-08-13 16:45:52 -0300

    feat: a rota de acesso negado foi adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,6 +26,12 @@ export const routes: Routes = [
 
     },
     {
+        path: 'acesso-negado',
+        loadComponent: () =>
+            import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado)
+
+    },
+    {
         path:'login',
         loadComponent:() =>
             import('./features/login/login/login').then((m) => m.Login),
