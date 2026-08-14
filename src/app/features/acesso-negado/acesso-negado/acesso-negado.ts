c88e1a1cc8bcc2f73d48385d596f79583e13e740
gustavo machado <tadeboa793@gmail.com>
2026-08-14 15:33:57 -0300

    feat: adicionado  o import e o inject de Router

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/service/auth.service';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [RouterLink],
+  imports: [RouterLink, MatAnchor],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
 export class AcessoNegado {
   private authService = inject(AuthService); //! teste em produção
+  private router = inject(Router)
+
+
+
   sair = this.authService.logout();  
 }
