152bdf5774295c68b634e54d28846fcc444bb3a7
gustavo machado <tadeboa793@gmail.com>
2026-08-21 16:58:59 -0300

    feat: adicionado a persistencia de dados para quando o usuario sair de carrinho os produtos continuem salvos

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, computed, signal } from "@angular/core";
+import { Injectable, computed, signal, effect, PLATFORM_ID, inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 type PerfilUsuario = 'admin' | 'usuario'; 
 
@@ -14,21 +15,44 @@ type Usuario = {
 export class AuthService {
 
 
-    private usuario = signal<Usuario | null> (null);
-    private tokenJwt = signal<string | null> (null);
+    private usuario = signal<Usuario | null> (this.CarregarUsuarioSalvo());
+    private tokenJwt = signal<string | null> (this.carregarTokenSalvo());
 
 
-    usuarioLogado = computed(() => this.usuario() !== null);
+    usuarioLogado = computed(() => 
+        this.usuario() !== null && this.tokenJwt() !== null
+    );
+    private estarNoNavegador(): boolean{
+        return isPlatformBrowser(this.platformId)
+    }
+   
+    
+    private limparAuthSalvo(){}
+
     usuarioAtual = computed(() => this.usuario());
     estaLogado = computed(() => this.usuario() !== null);
     token = computed(() => this.tokenJwt());
     admin = computed(() => this.usuario()?.perfil ==='admin');
 
+    //!==================== persistencia auth====================
+
+    private platformId = inject(PLATFORM_ID);
+    private readonly chaveUsuario = 'usuario-storage'
+    private readonly chaveToken = 'token-storage';
+
+
     login (email: string, senha: string): boolean{
         if(!email || !senha){
             return false;
         }
+
         const perfil: PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario'
+
+        const usuarioLogado: Usuario = {
+            email,
+            perfil,
+        };
+
         const tokenSimulado = 
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
     'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
@@ -40,11 +64,16 @@ export class AuthService {
         perfil,
     });
     this.tokenJwt.set(tokenSimulado);
+    this.usuario.set(usuarioLogado)
+    this.salvarAutenticacao(usuarioLogado, tokenSimulado)
+    //! VOLTAR DEPOIS
     return true;
 }
 logout(){
     this.usuario.set(null);
     this.tokenJwt.set(null);
+
+    this.limparAutenticacaoSalva();
 }
     obterToken(): string | null{
         return this.tokenJwt();
@@ -53,4 +82,44 @@ logout(){
         return this.usuario()?.perfil ?? null;
     }
 
+private estaNoNavegador(): boolean{
+    return isPlatformBrowser(this.platformId);
+}
+
+private CarregarUsuarioSalvo(): Usuario | null {
+    if(!this.estarNoNavegador()){
+        return null;
+    }
+    const dadosSalvos = localStorage.getItem(this.chaveUsuario);
+    if(!dadosSalvos) {
+        return null;
+    }try{
+        return JSON.parse(dadosSalvos) as Usuario
+    }catch{
+        return null;
+    }
+}
+private carregarTokenSalvo(): string | null {
+    if(!this.estarNoNavegador()){
+        return null;
+    }
+    return localStorage.getItem(this.chaveToken);
+    
+}
+
+
+    private salvarAutenticacao(usuario: Usuario, token: string){
+    return;
+    localStorage.setItem(this.chaveUsuario, JSON.stringify(usuario));
+    localStorage.setItem(this.chaveToken, (token));
+    }
+    private limparAutenticacaoSalva(){
+        if(!this.estarNoNavegador()){
+            return;
+        }
+
+        localStorage.removeItem(this.chaveUsuario)
+        localStorage.removeItem(this.chaveToken)
+    
+    }
 }
